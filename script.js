d52574d9bf151661679bfbeb44356b51d5780845
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2024-02-05 15:21:06 +1000

    init commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let place = "brisbane";
+let locData = {};
+fetchData();
+
+async function fetchData() {
+  await getRawLocData(place);
+  console.log(locData);
+}
+
+async function getRawLocData(location) {
+  const response = await fetch(
+    "http://api.weatherapi.com/v1/current.json?key=adbb17956b384bcfadc00937240502&q=" +
+      location
+  );
+
+  const dataPull = await response.json();
+
+  locData.location = {
+    name: dataPull.location.name,
+    region: dataPull.location.region,
+  };
+  locData.current = {
+    temp_c: dataPull.current.temp_c,
+    is_day: dataPull.current.is_day,
+    text: dataPull.current.condition.text,
+    icon: dataPull.current.condition.icon,
+  };
+}
