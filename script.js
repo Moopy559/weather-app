dcd72fe9115e7f10a247accc636bbab88635179f
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2024-02-07 21:51:09 +1000

    Add minor component - Created a 4 div grid for minor stats like sunrise/sunset, UV etc - Pasted the API fetch request as a comment in script.js to be worked on later

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let place = "brisbane";
-let locData = {};
+let locWeatherData = {};
+let locAstronomyData = {};
 const searchBar = document.getElementById("searchBar");
 const searchBtn = document.getElementById("searchBtn");
 const temp = document.querySelector(".stats h2");
@@ -10,10 +11,11 @@ fetchData();
 
 async function fetchData() {
   await getRawLocData(place);
-  console.log(locData);
+  console.log(locWeatherData);
   updateMainComponent();
 }
 
+// Calls Weather API
 async function getRawLocData(location) {
   const response = await fetch(
     "http://api.weatherapi.com/v1/current.json?key=adbb17956b384bcfadc00937240502&q=" +
@@ -22,17 +24,38 @@ async function getRawLocData(location) {
 
   const dataPull = await response.json();
 
-  locData.location = {
+  locWeatherData.location = {
     name: dataPull.location.name,
     region: dataPull.location.region,
   };
-  locData.current = {
+  locWeatherData.current = {
     temp_c: dataPull.current.temp_c,
     is_day: dataPull.current.is_day,
     text: dataPull.current.condition.text,
     icon: dataPull.current.condition.icon,
   };
 }
+
+// // Calls Astronomy API
+// async function getRawLocData() {
+//   const response = await fetch(
+//     // TASK: Change the below URL to have a dynamic date at the end
+//     "http://api.weatherapi.com/v1/astronomy.json?key=adbb17956b384bcfadc00937240502&q&q=brisbane&dt=2024-02-07"
+//   );
+
+//   const dataPull = await response.json();
+
+//   locAstronomyData.location = {
+//     name: dataPull.location.name,
+//     region: dataPull.location.region,
+//   };
+//   locAstronomyData.current = {
+//     temp_c: dataPull.current.temp_c,
+//     is_day: dataPull.current.is_day,
+//     text: dataPull.current.condition.text,
+//     icon: dataPull.current.condition.icon,
+//   };
+// }
 
 // Takes user input (location) and pulls weather data for that location
 searchBtn.addEventListener("click", () => {
@@ -41,9 +64,9 @@ searchBtn.addEventListener("click", () => {
 });
 
 function updateMainComponent() {
-  temp.textContent = `${locData.current.temp_c}\u00B0`;
-  summary.textContent = locData.current.text;
-  coords.textContent = `${locData.location.name}, ${locData.location.region}`;
-  icon.setAttribute("src", locData.current.icon);
-  icon.setAttribute("alt", `'${locData.current.text}' Icon`);
+  temp.textContent = `${locWeatherData.current.temp_c}\u00B0`;
+  summary.textContent = locWeatherData.current.text;
+  coords.textContent = `${locWeatherData.location.name}, ${locWeatherData.location.region}`;
+  icon.setAttribute("src", locWeatherData.current.icon);
+  icon.setAttribute("alt", `'${locWeatherData.current.text}' Icon`);
 }
